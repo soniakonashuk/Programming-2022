5fe697c2da7cd8b16a36c7e31d1e93f2f14bc381
Sonia K <s.konashuk@icloud.com>
2022-10-18 13:44:03 +0200

    refactor folder structure

---
+++ classwork 12.10/Source.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+/*int main() {
+	for (int i = 0; i <= 100; i++)
+	{
+		cout << i << "G" << 9.0 / 5 * i + 32 << "F" << endl;
+	}
+	return 0 
+}*/
+/*int main() {
+	int m;
+	cout << "Enter m= ";
+	cin >> m;
+	int k = 0;
+	while (pow(4, k) < m)
+	{
+		k = k + 1;
+	}
+	cout << "k=" << k - 1 << endl;
+	cout << pow(4, k - 1) << endl;
+	cout << pow(4, k) << endl;
+
+	return 0;
+}*/
+/*int main() {
+	int n;
+	cout << "Enter n=";
+	cin >> n;
+	int u0 = 0, u1= 1, u;
+	cout << uo << endl;
+	cout << u1 << endl;
+	for (int i; i = 2; i <= n; i++)
+	{
+		u = u0 + u1;
+		cout << u << endl;
+		uo = u1;
+		u1 = u;
+
+	}
+	return 0;
+}*/
+unsigned long long n;
+long long a;
+cin >> n;
+while (n > 0) {
+	n % 10;
+	n /= 10;
+	b *= 10
+		b + a;
+}
+cout << b << endl;
