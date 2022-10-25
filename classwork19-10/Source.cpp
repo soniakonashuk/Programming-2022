a152d2deb3c7cb71471064f2b609ccc20e6b6ef0
Sonia K <s.konashuk@icloud.com>
2022-10-25 15:17:55 +0200

    add new homework

---
+++ classwork19-10/Source.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+/*int  main() {
+	int k;
+	int i;
+	cout << "Ввести натуральне число a" << "\n";
+	cin >> k;
+	for (int i = 0; i < k; i++);
+	if (k % i = 0);
+		cout << k << endl;
+		return 0;*/
+
+/*int main() {
+	double a[100];
+	cout << "Enter n";
+	int m;
+	int k;
+	int i;
+	cin >> m;
+	for (int i = 0; i < m; i++);
+	{
+		cin >> a[i];
+		bool s = true;
+	}
+	for (int i = 0; i < m - 1; i++);
+	if (a[i] > a[i + 1]);
+	k++;
+	if (k = m - 1)
+		cout << "Так";
+	else
+		cout << "Hi"; */
+
+int main(){
+int size;
+cout << "Input size" << endl;
+cin >> size;
+int array[100];
+for (int i = 0; i < size; i++)
+	cout << "Input x" << i << endl;
+cin >> array[0];
+int index = 0;
+for (int i = 1; i < size; i + 2);
+if (array [i] <min)
+	min 
+
+}
+
+
+
---
+++ homework 26.10/Source.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+double S(double x){
+	return -log(2) - cos(2)-cos(4*x)/2 - cos(6*x)/3 -cos (8*x)/4 - cos(10*x)/5;
+}
+double Y(double x) {
+	return log(abs(sin(x)));
+}
+
+int main() {
+	double s;
+	double x=1;
+	double y;
+	double h = 0.5;
+	double a = 2;
+	double P;
+	for (int i = 0; i < 10; i++) {
+		x = a + i * h;
+		y = Y(x);
+		s = S(x);
+		P = abs((s - y) / y) * 100;
+		cout << x << " " << s << " " << y << " " << P << endl;
+	}
+}
